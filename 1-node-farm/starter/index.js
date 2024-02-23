30efd84f8214bc8ed31fdff011a9e508ec1de8b6
Gayathri Mantha <gayathrimantha16@gmail.com>
2024-02-23 19:50:02 +0530

    initial

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,2 +1,10 @@
 const hello = "Hello There";
 console.log(hello);
+
+const fs = require("fs");
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+const textOut = `Test : ${textIn}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("Written");
